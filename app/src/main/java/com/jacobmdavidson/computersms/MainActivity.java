ec058c6e48cda360089ff2fe00d4e20827f83dd6
Jacob Davidson <jacobdavidson79@gmail.com>
2015-08-05 18:06:16 -0500

    MainActivity functionality is done. It now has a toggle button that enables/disables the MessageReceiver.

--- app/src/main/java/com/jacobmdavidson/computersms/MainActivity.java
+++ app/src/main/java/com/jacobmdavidson/computersms/MainActivity.java
@@ -1,37 +1,77 @@
 package com.jacobmdavidson.computersms;
 
+import android.content.ComponentName;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    // The enable/disable toggle button
+    private ToggleButton toggle;
+
+    // Shared preferences that stores the toggle state
+    private SharedPreferences sharedPrefs;
+
+    // Shared preferences editor
+    private SharedPreferences.Editor editor;
+
+    // Package Manager used to register/deregister the MessageReceiver
+    private PackageManager pm;
+
+    // Component to register/deregister
+    private ComponentName component;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the saved preferences
+        sharedPrefs = getSharedPreferences("com.jacobmdavidson.computersms", MODE_PRIVATE);
+        editor = sharedPrefs.edit();
+
+        // Instantiate and Set the toggle button according to the preferences
+        toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        toggle.setChecked(sharedPrefs.getBoolean("ToggleButtonState", false));
+
+        // get the package manager, and instantiate the component name
+        pm = this.getPackageManager();
+        component = new ComponentName(this, MessageReceiver.class);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * Update the shared prefs and enable/disable the MessageReceiver when the
+     * toggle button is pressed.
+     * @param view the view from which the click is received
+     */
+    public void onToggleClicked(View view) {
+        boolean enabled = ((ToggleButton) view).isChecked();
+        if (enabled) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Update the shared prefs
+            editor.putBoolean("ToggleButtonState", true);
+            editor.commit();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Enable the receiver
+            pm.setComponentEnabledSetting(component,
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                    PackageManager.DONT_KILL_APP);
+        } else {
+
+            // Update the shared prefs
+            editor.putBoolean("ToggleButtonState", false);
+            editor.commit();
+
+            // Disable the receiver
+            pm.setComponentEnabledSetting(component,
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jacobmdavidson/computersms/MessageReceiver.java
+++ app/src/main/java/com/jacobmdavidson/computersms/MessageReceiver.java
@@ -3,6 +3,7 @@ package com.jacobmdavidson.computersms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by jacobdavidson.
@@ -12,6 +13,5 @@ public class MessageReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
     }
 }
